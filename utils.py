c7147d7ac3b9d26e55aabab6b0ff1c143e542673
mauriciogtec <mauriciogtec@hsph.harvard.edu>
2022-10-21 14:20:55 -0400

    improve seeds

--- utils.py
+++ utils.py
@@ -13,6 +13,8 @@ def set_seed(seed: int):
     np.random.seed(seed)
     random.seed(seed)
     torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
 
 
 def make_logger(
